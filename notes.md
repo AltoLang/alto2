f135bf1010ab4f52d4303fb54aa5aba629b72324
FilipToth <filip.toth@accelley.com>
2023-02-12 16:17:55 -0500

    Very basic pest expression parser

---
+++ notes.md
@@ -0,0 +1,1 @@
+https://pest.rs/book/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,74 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+extern crate lazy_static;
+
+use std::io;
+use pest::{Parser, pratt_parser::PrattParser, iterators::Pairs};
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct AltoParser;
+
+#[derive(Debug)]
+pub enum Expr {
+    NumberToken(i32),
+    BinOp {
+        lhs: Box<Expr>,
+        op: Op,
+        rhs: Box<Expr>
+    }
+}
+
+#[derive(Debug)]
+pub enum Op {
+    Add,
+    Subtract
+}
+
+lazy_static::lazy_static! {
+    static ref PRATT_PARSER: PrattParser<Rule> = {
+        use pest::pratt_parser::{Assoc::*, Op};
+        use Rule::*;
+
+        // the `op` call order determines operation precedences
+        PrattParser::new()
+            .op(Op::infix(add, Left) | Op::infix(subtract, Left))
+    };
+}
+
+pub fn parse_expression(pairs: Pairs<Rule>) -> Expr {
+    PRATT_PARSER
+        .map_primary(|primary| match primary.as_rule() {
+            Rule::number_token => { println!("prim: {:?}", primary.as_str()); Expr::NumberToken(primary.as_str().parse::<i32>().unwrap()) },
+            rule => unreachable!("Expr::parse expects an atom, found {:?}", rule)
+        })
+        .map_infix(|lhs, op, rhs| {
+            let op = match op.as_rule() {
+                Rule::add => Op::Add,
+                Rule::subtract => Op::Subtract,
+                rule => unreachable!("Expected an infix operation, got '{:?}'", rule)
+            };
+            Expr::BinOp { lhs: Box::new(lhs), op, rhs: Box::new(rhs) }
+        })
+        .parse(pairs)
+}
+
 fn main() {
-    println!("Hello, world!");
+    for line in io::stdin().lines() {
+        let ln = line.unwrap();
+        match AltoParser::parse(Rule::expression, &ln) {
+            Ok(mut pairs) => {
+                println!(
+                    "Parsed: {:#?}",
+                    parse_expression(
+                        pairs.next().unwrap().into_inner()
+                    )
+                );
+            }
+            Err(e) => {
+                eprintln!("Parse failed: {:?}", e)
+            }
+        }
+    }
 }
