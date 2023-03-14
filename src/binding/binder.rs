4b0c3a7b37f4da7e2d8b14270431469ff639c112
FilipToth <filip.toth@accelley.com>
2023-03-14 07:06:55 -0400

    Bind variable declarations

--- src/binding/binder.rs
+++ src/binding/binder.rs
@@ -27,6 +27,10 @@ pub enum BoundNode {
     AssignmentExpression {
         identifier: String,
         expression: Box<BoundNode>
+    },
+    DeclarationStatement {
+        identifier: String,
+        expression: Box<BoundNode>
     }
 }
 
@@ -68,10 +72,26 @@ fn bind_bin_expression(lhs: SyntaxToken, op: Op, rhs: SyntaxToken) -> BoundNode 
 }
 
 fn bind_assignment_expression(identifier: SyntaxToken, expression: SyntaxToken) -> BoundNode {
-
     if let SyntaxToken::IdentifierToken(str) = identifier {
         let bound_expr = bind(expression);
+        if get_type(&bound_expr) == Type::Void {
+            panic!("Cannot assign void to '{:?}'", str)
+        }
+
         BoundNode::AssignmentExpression { identifier: str, expression: Box::new(bound_expr) }
+    } else {
+        panic!("Cannot assign to: '{:?}'", identifier)
+    }
+}
+
+fn bind_declaration_statement(identifier: SyntaxToken, expression: SyntaxToken) -> BoundNode {
+    if let SyntaxToken::IdentifierToken(str) = identifier {
+        let bound_expr = bind(expression);
+        if get_type(&bound_expr) == Type::Void {
+            panic!("Cannot assign void to '{:?}'", str)
+        }
+
+        BoundNode::DeclarationStatement { identifier: str, expression: Box::new(bound_expr) }
     } else {
         panic!("Cannot assign to: '{:?}'", identifier)
     }
@@ -83,7 +103,8 @@ fn get_type(node: &BoundNode) -> Type {
         BoundNode::StringLiteral(..) => Type::String,
         BoundNode::RootStatement {..} => Type::Void,
         BoundNode::BinExpression { lhs: _, op: _, rhs: _, tp } => tp.clone(),
-        BoundNode::AssignmentExpression { identifier: _, expression } => get_type(expression)
+        BoundNode::AssignmentExpression { identifier: _, expression } => get_type(expression),
+        BoundNode::DeclarationStatement { identifier: _, expression: _ } => Type::Void
     }
 }
 
@@ -94,6 +115,7 @@ pub fn bind(token: SyntaxToken) -> BoundNode {
         SyntaxToken::StringToken(str) => { bind_string_token(str) },
         SyntaxToken::BinExpression { lhs, op, rhs } => { bind_bin_expression(*lhs, op, *rhs) },
         SyntaxToken::AssignmentExpression { identifier, expression } => { bind_assignment_expression(*identifier, *expression) }
+        SyntaxToken::DeclarationStatement { identifier, expression } => { bind_declaration_statement(*identifier, *expression) }
         _ => unreachable!("Unknown token: '{:?}'", token)
     }
 }
