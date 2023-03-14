25228a9b4b0ac42f33d095c907369134f9df9e00
FilipToth <filip.toth@accelley.com>
2023-03-14 06:12:00 -0400

    Bind string literals and add basic binary expression type checking

--- src/binding/binder.rs
+++ src/binding/binder.rs
@@ -12,44 +12,72 @@ QUICK NOTE:
 */
 
 #[derive(Debug)]
-pub enum BoundToken {
-    NumberToken(i32),
+pub enum BoundNode {
+    NumberLiteral(i32),
+    StringLiteral(String),
     RootStatement {
-        tokens: Box<Vec<BoundToken>>
+        tokens: Box<Vec<BoundNode>>
     },
     BinExpression {
-        lhs: Box<BoundToken>,
+        lhs: Box<BoundNode>,
         op: Op,
-        rhs: Box<BoundToken>
+        rhs: Box<BoundNode>,
+        tp: Type
     }
 }
 
-fn bind_root_statement(tokens: Vec<SyntaxToken>) -> BoundToken {
+#[derive(Debug, PartialEq, Clone)]
+pub enum Type {
+    Number,
+    String,
+    Void
+}
+
+fn bind_root_statement(tokens: Vec<SyntaxToken>) -> BoundNode {
     let mut bounded = Vec::new();
     for t in tokens {
         bounded.push(bind(t));
     }
 
-    BoundToken::RootStatement { tokens: Box::new(bounded) }
+    BoundNode::RootStatement { tokens: Box::new(bounded) }
 }
 
-fn bind_bin_expression(lhs: SyntaxToken, op: Op, rhs: SyntaxToken) -> BoundToken {
+fn bind_bin_expression(lhs: SyntaxToken, op: Op, rhs: SyntaxToken) -> BoundNode {
     let left = bind(lhs);
     let right = bind(rhs);
 
-    BoundToken::BinExpression { lhs: Box::new(left), op: op, rhs: Box::new(right) }
+    let left_type = get_type(&left);
+    let right_type = get_type(&right);
+    if left_type != right_type {
+        panic!("Undefined binary operator for types left: '{:?}' and right: '{:?}'", left_type, right_type);
+    }
+
+    BoundNode::BinExpression { lhs: Box::new(left), op: op, rhs: Box::new(right), tp: left_type }
 }
 
-fn bind_number_token(num: i32) -> BoundToken {
-    BoundToken::NumberToken(num)
+fn bind_number_token(num: i32) -> BoundNode {
+    BoundNode::NumberLiteral(num)
 }
 
+fn bind_string_token(str: String) -> BoundNode {
+    BoundNode::StringLiteral(str)
+}
 
-pub fn bind(token: SyntaxToken) -> BoundToken {
+fn get_type(node: &BoundNode) -> Type {
+    match node {
+        BoundNode::NumberLiteral(..) => Type::Number,
+        BoundNode::StringLiteral(..) => Type::String,
+        BoundNode::RootStatement {..} => Type::Void,
+        BoundNode::BinExpression { lhs: _, op: _, rhs: _, tp } => tp.clone(),
+    }
+}
+
+pub fn bind(token: SyntaxToken) -> BoundNode {
     match token {
         SyntaxToken::RootStatement { tokens } => { bind_root_statement(*tokens) }
         SyntaxToken::BinExpression { lhs, op, rhs } => { bind_bin_expression(*lhs, op, *rhs) },
         SyntaxToken::NumberToken(num) => { bind_number_token(num) },
-        _ => unreachable!("Unknown token {:?}", token)
+        SyntaxToken::StringToken(str) => { bind_string_token(str) },
+        _ => unreachable!("Unknown token: '{:?}'", token)
     }
 }
